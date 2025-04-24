08ac671ad725750675a422ff42f8e20bbde6d8c5
Jasper Tabin <tabinjasper@gmail.com>
2025-04-25 00:15:54 +0800

    Added Chart.js & installed node.js, added the content for dashboard script files, contents, styling.

--- js/sidebar.js
+++ js/sidebar.js
@@ -15,20 +15,30 @@ document.addEventListener('DOMContentLoaded', () => {
             const target = item.querySelector('a').getAttribute('data-target');
             const contentContainer = document.getElementById('content-container');
 
-            fetch(target)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Failed to load content');
-                    }
-                    return response.text();
-                })
-                .then(html => {
-                    contentContainer.innerHTML = html;
-                })
-                .catch(error => {
-                    console.error('Error loading content:', error);
-                    contentContainer.innerHTML = '<p>Error loading content.</p>';
-                });
+            // Use iframe to load the content for the script
+            var iframe = document.createElement('iframe');
+            iframe.src = target;
+            iframe.style.width = '100%';    
+            iframe.style.height = '100%';
+            
+
+            contentContainer.innerHTML = ''; 
+            contentContainer.appendChild(iframe);
+            
+            // fetch(target)
+            //     .then(response => {
+            //         if (!response.ok) {
+            //             throw new Error('Failed to load content');
+            //         }
+            //         return response.text();
+            //     })
+            //     .then(html => {
+            //         contentContainer.innerHTML = html;
+            //     })
+            //     .catch(error => {
+            //         console.error('Error loading content:', error);
+            //         contentContainer.innerHTML = '<p>Error loading content.</p>';
+            //     });
         });
     });
 });

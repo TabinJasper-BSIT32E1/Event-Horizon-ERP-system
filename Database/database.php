df04210acb115ee99468dc59efd1d3009b5a1640
Jasper Tabin <tabinjasper@gmail.com>
2025-05-02 01:25:50 +0800

    Added Database/sql files/functionality on employee tab(js/php/css)

---
+++ Database/database.php
@@ -0,0 +1,9 @@
+<?php
+    $db_server = "localhost";
+    $db_user = "root";
+    $db_pass = "";
+    $db_name = "event-horizon-erp-system"; // set dbname everytime you want to call database.php
+
+    $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+
+?>
